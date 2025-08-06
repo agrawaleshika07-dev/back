f9c4263e705b1808c8f743998d5567afe55ebd0f
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-06 17:41:18 +0530

    Backend code commit

---
+++ src/main/java/com/example/cld/MessageController.java
@@ -0,0 +1,26 @@
+package com.example.cld;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping
+    public void addMessage(@RequestBody String content) {
+        messageService.saveMessage(content);
+    }
+
+    @GetMapping
+    public List<String> getAllMessages() {
+        return messageService.getAllMessages();
+    }
+}
